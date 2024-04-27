be4b5e46ac049685e3eeb1be300c8454b24b7255
Jack Conner <1jackconner@gmail.com>
2024-04-27 07:36:04 -0700

    clean

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,16 +2,3 @@ require("config.options")
 require("config.keymaps")
 require("config.autocmds")
 require("config.lazy")
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
