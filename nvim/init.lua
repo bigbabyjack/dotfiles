0f2086df64cfc56eae3e0584fa372a35efcdc9dd
Jack Conner <1jackconner@gmail.com>
2024-04-27 07:19:10 -0700

    begin modularization

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,15 +1,7 @@
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")
--- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-end ---@diagnostic disable-next-line: undefined-field
-vim.opt.rtp:prepend(lazypath)
-
+require("config.lazy")
 -- [[ Configure and install plugins ]]
 --
 --  To check the current status of your plugins, run
@@ -24,7 +16,6 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
-	"Exafunction/codeium.vim",
 	-- NOTE: Plugins can also be added by using a table,
 	-- with the first argument being the link and the following
 	-- keys can be used to configure plugin behavior/loading/etc.
