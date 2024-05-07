6d2de204e4b35cf9ef26ccf128224a5caad0e235
Jack Conner <1jackconner@gmail.com>
2024-05-06 18:41:45 -0700

    add ts

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -412,6 +412,7 @@ require("lazy").setup({
 				ruff_lsp = {},
 				ruff = {},
 				pyright = {},
+				tsserver = {},
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
