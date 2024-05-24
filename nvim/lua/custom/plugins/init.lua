078bd96c04455e7e25121369f703b7d8d9e8611f
Jack Conner <1jackconner@gmail.com>
2024-05-23 20:28:54 -0700

    zenmode

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -22,11 +22,33 @@ return {
 			"folke/twilight.nvim",
 			opts = {
 				wezterm = {
-					enabled = false,
+					enabled = true,
 					-- can be either an absolute font size or the number of incremental steps
 					font = "+4", -- (10% increase per step)
 				},
 			},
+		},
+		{
+			"stevearc/oil.nvim",
+			dependencies = { "nvim-tree/nvim-web-devicons" },
+			config = function()
+				require("oil").setup({
+					columns = { "icon" },
+					keymaps = {
+						["<C-h>"] = false,
+						["<M-h>"] = "actions.select_split",
+					},
+					view_options = {
+						show_hidden = true,
+					},
+				})
+
+				-- Open parent directory in current window
+				vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+				-- Open parent directory in floating window
+				vim.keymap.set("n", "<space>-", require("oil").toggle_float)
+			end,
 		},
 	},
 }
