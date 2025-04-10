3e24d520792f81bc988eef2611d9a53d55d9443b
Jack Conner <1jackconner@gmail.com>
2025-04-09 17:00:21 -0700

    add keymaps for floaterminal

--- nvim/lua/custom/plugins/floaterminal.lua
+++ nvim/lua/custom/plugins/floaterminal.lua
@@ -1,5 +1,3 @@
-vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
-
 local state = {
   floating = {
     buf = -1,
@@ -39,7 +37,7 @@ local function create_floating_window(opts)
   local win = vim.api.nvim_open_win(buf, true, win_config)
 
   -- Set the background color for the terminal window
-  vim.api.nvim_win_set_option(win, "winhl", "Normal:TermBackground")
+  vim.api.nvim_win_set_option(win, "winhl", "Normal:TermBackground,FloatBorder:TermBackground")
 
   return { buf = buf, win = win }
 end
@@ -51,8 +49,16 @@ local toggle_terminal = function()
     if vim.bo[state.floating.buf].buftype ~= "terminal" then
       vim.cmd.terminal()
     end
+    -- Automatically enter insert mode when terminal is opened
+    vim.cmd('startinsert')
   else
+    -- Store the mode before hiding the window
+    local is_in_insert_mode = vim.api.nvim_get_mode().mode:match('i')
     vim.api.nvim_win_hide(state.floating.win)
+    -- If we were in insert mode, restore it for the next buffer
+    if is_in_insert_mode then
+      vim.defer_fn(function() vim.cmd('stopinsert') end, 10)
+    end
   end
 end
 
@@ -61,6 +67,14 @@ end
 vim.api.nvim_create_user_command("Floaterminal", toggle_terminal, {})
 
 -- Set up a key mapping to toggle the terminal
-vim.keymap.set("n", "<leader>ft", function()
+vim.keymap.set("n", "<leader>tt", function()
   toggle_terminal()
 end, { desc = "Toggle floating terminal" })
+
+vim.keymap.set("t", "<Esc><Esc>", function()
+  if vim.api.nvim_win_is_valid(state.floating.win) then
+    vim.api.nvim_win_hide(state.floating.win)
+  end
+end, { desc = "Close floating terminal" })
+
+return {}
