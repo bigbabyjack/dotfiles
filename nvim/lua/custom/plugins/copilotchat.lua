8c5312ffb63185c21bf8071ca54f11e3702748dc
Jack Conner <1jackconner@gmail.com>
2025-05-14 22:32:44 -0700

    copilot canceled

--- nvim/lua/custom/plugins/copilotchat.lua
+++ nvim/lua/custom/plugins/copilotchat.lua
@@ -1,24 +1,24 @@
 return {
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    dependencies = {
-      { "zbirenbaum/copilot.lua" },                   -- or zbirenbaum/copilot.lua
-      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
-    },
-    build = "make tiktoken",                          -- Only on MacOS or Linux
-    opts = {
-      window = {
-        layout = 'float',
-        border = 'rounded'
-      }
-    },
-    -- vim.keymap.set(
-    --   'n',
-    --   '<leader>cc',
-    --   function()
-    --     require("CopilotChat").toggle()
-    --   end,
-    --   { noremap = true, silent = true, desc = "Toggle Copilot Chat" }
-    -- )
-  },
+  -- {
+  --   "CopilotC-Nvim/CopilotChat.nvim",
+  --   dependencies = {
+  --     { "zbirenbaum/copilot.lua" },                   -- or zbirenbaum/copilot.lua
+  --     { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+  --   },
+  --   build = "make tiktoken",                          -- Only on MacOS or Linux
+  --   opts = {
+  --     window = {
+  --       layout = 'float',
+  --       border = 'rounded'
+  --     }
+  --   },
+  --   -- vim.keymap.set(
+  --   --   'n',
+  --   --   '<leader>cc',
+  --   --   function()
+  --   --     require("CopilotChat").toggle()
+  --   --   end,
+  --   --   { noremap = true, silent = true, desc = "Toggle Copilot Chat" }
+  --   -- )
+  -- },
 }
