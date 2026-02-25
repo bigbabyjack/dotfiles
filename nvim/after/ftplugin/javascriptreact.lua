a7bb5d4afb267e265440309624739648154f55cf
Jack Conner <1jackconner@gmail.com>
2026-02-24 21:13:34 -0800

    fix: add expandtab to web ftplugins, declare autotag treesitter dependency

--- nvim/after/ftplugin/javascriptreact.lua
+++ nvim/after/ftplugin/javascriptreact.lua
@@ -1,3 +1,5 @@
+-- Override global 4-space indent with 2-space for web files
 vim.opt_local.tabstop = 2
 vim.opt_local.softtabstop = 2
 vim.opt_local.shiftwidth = 2
+vim.opt_local.expandtab = true
--- nvim/lua/custom/plugins/webdev.lua
+++ nvim/lua/custom/plugins/webdev.lua
@@ -24,6 +24,7 @@ return {
   {
     'windwp/nvim-ts-autotag',
     event = { 'BufReadPre', 'BufNewFile' },
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
     opts = {},
   },
 }
