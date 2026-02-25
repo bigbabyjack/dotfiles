5a4d7d9bcbe4a48161d7dbf74142d6357a7cac25
Jack Conner <1jackconner@gmail.com>
2026-02-24 21:09:23 -0800

    feat: add conform/prettier, nvim-ts-autotag, and 2-space indent for web filetypes

---
+++ nvim/lua/custom/plugins/webdev.lua
@@ -0,0 +1,29 @@
+return {
+  -- Prettier formatting via conform.nvim
+  {
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    opts = {
+      formatters_by_ft = {
+        javascript = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        typescript = { 'prettier' },
+        typescriptreact = { 'prettier' },
+        html = { 'prettier' },
+        css = { 'prettier' },
+        scss = { 'prettier' },
+      },
+      format_on_save = {
+        timeout_ms = 2000,
+        lsp_fallback = false, -- Don't fall back to LSP; Prettier or nothing
+      },
+    },
+  },
+
+  -- Auto-close and auto-rename HTML/JSX tags
+  {
+    'windwp/nvim-ts-autotag',
+    event = { 'BufReadPre', 'BufNewFile' },
+    opts = {},
+  },
+}
