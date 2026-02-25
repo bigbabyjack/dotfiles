e819f4003c0624f3c8c4d3a1359ea306faca67d0
Jack Conner <1jackconner@gmail.com>
2026-02-24 21:00:28 -0800

    feat: add mason-tool-installer with web dev binaries

--- nvim/lua/custom/plugins/mason.lua
+++ nvim/lua/custom/plugins/mason.lua
@@ -1,5 +1,20 @@
 return {
-  'williamboman/mason.nvim',
-  cmd = 'Mason',
-  opts = {},
+  {
+    'williamboman/mason.nvim',
+    cmd = 'Mason',
+    opts = {},
+  },
+  {
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    opts = {
+      ensure_installed = {
+        -- Web dev servers (added now)
+        'typescript-language-server',
+        'eslint-lsp',
+        'vscode-langservers-extracted', -- provides html, cssls, jsonls
+        'tailwindcss-language-server',
+        'prettier',
+      },
+    },
+  },
 }
