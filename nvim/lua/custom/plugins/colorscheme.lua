5ab80c5ec8bcb8109081e6f3278341688c8a188c
Jack Conner <1jackconner@gmail.com>
2026-05-03 18:38:50 -0700

    refactor(nvim): modernize deprecated APIs
    - vim.loop.fs_stat -> vim.uv.fs_stat (vim.loop is a deprecated alias)
    - lazy.nvim plugin spec: as= -> name= (renamed in lazy.nvim 10+)

--- nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/lua/custom/plugins/colorscheme.lua
@@ -1,57 +1,58 @@
-return { {
-  'sainnhe/gruvbox-material',
-  lazy = true,
-  priority = 1000,
-  opts = {
-    integrations = {
-      cmp = true,
-      gitsigns = true,
-      nvimtree = true,
-      treesitter = true,
-    },
-  }
-},
+return {
   {
-    "folke/tokyonight.nvim",
+    'sainnhe/gruvbox-material',
     lazy = true,
     priority = 1000,
     opts = {
-      style = "day",
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+      },
+    },
+  },
+  {
+    'folke/tokyonight.nvim',
+    lazy = true,
+    priority = 1000,
+    opts = {
+      style = 'day',
       transparent = false,
       styles = {
         comments = { italic = true },
         keywords = { italic = true },
-      }
+      },
     },
   },
   {
-    "rose-pine/neovim",
-    name = "rose-pine",
+    'rose-pine/neovim',
+    name = 'rose-pine',
     lazy = true,
     priority = 1000,
     opts = {
-      variant = "moon",
+      variant = 'moon',
       disable_italics = true,
       styles = {
         comments = { italic = true },
         keywords = { italic = true },
         functions = { italic = false },
         variables = { italic = false },
-      }
+      },
     },
   },
   {
-    "catppuccin/nvim",
-    as = "catppuccin",
+    'catppuccin/nvim',
+    name = 'catppuccin',
     lazy = true,
     priority = 1000,
     opts = {
-      flavour = "mocha", -- picks the warmest tones
+      flavour = 'mocha', -- picks the warmest tones
       integrations = {
         treesitter = true,
         telescope = true,
         native_lsp = { enabled = true },
       },
-    }
-  }
+    },
+  },
 }
